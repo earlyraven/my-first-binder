fc6213e4441c4b29d9cc7b5449a6c88167e13001
earlyraven <raven.earl@gmail.com>
2023-04-06 04:18:01 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
+import numpy
+
 print("Greetings, Earthling!  Binder welcomes you!")
+Print("Let's get you a numpy generated random number.")
+print("Using version: ", numpy.__version__)
+print("Here's your random number:")
+numpy.random.randn()
